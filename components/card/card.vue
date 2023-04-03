76580e4fb2a0b9cd1af958749094148a91f8f2b7
haoshuo <18811467730@163.com>
2023-04-03 15:13:45 +0800

    init

---
+++ components/card/card.vue
@@ -0,0 +1,58 @@
+<template>
+	<view class="box">
+		<cover-image 
+			class="item" 
+			v-for="(item, idx) in list" 
+			:src="item.url" 
+			@touchmove.stop.prevent="touchMove(idx,$event)"
+			@touchend="touchEnd(idx,$event)"
+			:style="{left:item.left+'rpx',top:item.top+'rpx','z-index':item.zIndex}"
+		>
+		</cover-image>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"card",
+		props:{
+			list:{
+				type:Array,
+				default:[],
+			}
+		},
+		data() {
+			return {
+
+			};
+		},
+		methods:{
+			touchMove(idx, e) {
+				console.log(idx,e,e.changedTouches[0].pageX,e.changedTouches[0].pageY);
+				const touchData=e.changedTouches[0];
+				this.$set(this.list[idx], "z-index", 999);
+				// this.$set(this.list[idx], "left", e.changedTouches[0].pageX-347);
+				// this.$set(this.list[idx], "top", e.changedTouches[0].pageY-144);
+				this.$set(this.list[idx], "left", touchData.pageX-347+touchData.clientX);
+				this.$set(this.list[idx], "top", touchData.pageY-288-40+touchData.clientY);
+				
+			},
+			touchEnd(idx, e) {
+				console.log('end');
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.box {
+		position: relative;
+		background-color: palevioletred;
+	}
+	.item {
+		position: absolute;
+		width: 694rpx;
+		height: 288rpx;
+		border-radius: 30rpx;
+	}
+</style>
---
+++ pages/tabbar/tabbar-1/tabbar-1.vue
@@ -0,0 +1,48 @@
+<template>
+	<view class="content">
+		<card :list="imageList"></card>
+	</view>
+</template>
+
+<script>
+import card from "../../../components/card/card.vue";
+
+import img1 from '../../../images/1.png';
+import img2 from '../../../images/2.png';
+import img3 from '../../../images/3.png';
+import img4 from '../../../images/4.png';
+import img5 from '../../../images/5.png';
+import img6 from '../../../images/6.png';
+import img7 from '../../../images/7.png';
+import img8 from '../../../images/8.png';
+export default {
+	components:{
+		card
+	},
+	data() {
+		return {
+			title: 'Hello',
+			imageList:[
+				{name:"img1",url:img1,left:0,top:40,zIndex:3},
+				{name:"img2",url:img2,left:20,top:20,zIndex:2},
+				{name:"img3",url:img3,left:40,top:0,zIndex:1},
+				{name:"img4",url:img4,left:40,top:0,zIndex:1},
+				{name:"img5",url:img5,left:40,top:0,zIndex:1},
+				{name:"img6",url:img6,left:40,top:0,zIndex:1},
+				{name:"img7",url:img7,left:40,top:0,zIndex:1},
+				{name:"img8",url:img8,left:40,top:0,zIndex:1},
+			],
+		};
+	},
+	onLoad() {},
+	methods: {}
+};
+</script>
+
+<style>
+.content {
+	text-align: center;
+	height: 400upx;
+	margin-top: 200upx;
+}
+</style>
